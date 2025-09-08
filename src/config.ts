522f91d09e08e881416e01227bb3f77289ded301
Dumitru C. <canteadumitru@gmail.com>
2025-09-08 11:59:48 +0300

    quotes=true

--- src/config.ts
+++ src/config.ts
@@ -16,6 +16,6 @@ export const Config = {
       country: "ro"
     },
     demoText:
-      'Președintele Statelor Unite, Donald Trump, a sugerat, luni, că viitoarea sa întâlnire cu liderul regimului de la Phenian, Kim Jong-un, ar putea avea loc în "Casa Păcii", situată la granița dintre Coreea de Nord și Coreea de Sud, relatează site-ul agenției Yonhap'
+      'Președintele Statelor Unite, Donald Trump, a sugerat, luni, că viitoarea sa întâlnire cu liderul regimului de la Phenian, Kim Jong-un, ar putea avea loc în "Casa Păcii", situată la granița dintre Coreea de Nord și Coreea de Sud, relatează site-ul agenției Yonhap. Trump a spus ca "Eu am făcut cea mai frumoasă înțelegere!"'
   }
 };
